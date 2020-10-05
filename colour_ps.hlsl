c402bf5420d9789b85cb25a99112219db35c48c3
Patrick Carr <carr.patrickjames@gmail.com>
2020-10-05 16:58:38 +0100

    Add tutorial files

---
+++ colour_ps.hlsl
@@ -0,0 +1,14 @@
+// Colour pixel/fragment shader
+// Basic fragment shader outputting a colour
+
+struct InputType
+{
+	float4 position : SV_POSITION;
+	float4 colour : COLOR;
+};
+
+
+float4 main(InputType input) : SV_TARGET
+{
+	return input.colour;
+}
---
+++ colour_vs.hlsl
@@ -0,0 +1,40 @@
+// colour vertex shader
+// Simple geometry pass
+
+
+cbuffer MatrixBuffer : register(b0)
+{
+	matrix worldMatrix;
+	matrix viewMatrix;
+	matrix projectionMatrix;
+};
+
+struct InputType
+{
+	float4 position : POSITION;
+	float4 colour : COLOR;
+};
+
+struct OutputType
+{
+	float4 position : SV_POSITION;
+	float4 colour : COLOR;
+};
+
+OutputType main(InputType input)
+{
+	OutputType output;
+	
+	// Change the position vector to be 4 units for proper matrix calculations.
+	input.position.w = 1.0f;
+
+	// Calculate the position of the vertex against the world, view, and projection matrices.
+	output.position = mul(input.position, worldMatrix);
+	output.position = mul(output.position, viewMatrix);
+	output.position = mul(output.position, projectionMatrix);
+
+
+	output.colour = input.colour;
+
+	return output;
+}
